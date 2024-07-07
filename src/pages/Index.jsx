4fd91dd7d54ddea231878aa20a2a22906e42ed7f
luis1gonzalez <luis1gonzalez@hotmail.com>
2024-07-07 12:40:59 +0200

    quitando un header

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,10 +1,8 @@
 import Gif from "../components/Gif"
-import Header from "../components/Header"
 
 const Home = () => {
     return (
         <div className="krub-regular w-[320px]">
-            <Header />
             <Gif />
         </div>
     )
